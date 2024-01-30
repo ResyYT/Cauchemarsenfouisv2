d80557641d6ee98539c57301f81b5aee8bc15266
ResyYT <martin.schnidersylvestre@gmail.com>
2024-01-30 19:33:09 +0100

    Ajout multi (prototype)

--- scripts/Startup.gd
+++ scripts/Startup.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var _statusText = $Panel/InformationsStatus/StatusServeur
 @onready var _yourName = $Panel/InformationsConnection/Pseudo
 @onready var _connection = $Panel/BoutonsConnection/Connection
+var status_serveur = "Disline"
 var player_id_name = {}
 
 func _ready():
@@ -22,6 +23,7 @@ func startServeur():
 	var server = ENetMultiplayerPeer.new()
 	server.create_server(1111,4)
 	multiplayer.multiplayer_peer = server
+	status_serveur = "Online"
 	
 func startClient():
 	multiplayer.connected_to_server.connect(self.connected_to_server)
@@ -37,6 +39,16 @@ func infoConnectionsServer(info):
 	if multiplayer.is_server():
 		player_id_name[multiplayer.get_unique_id()] = str(info)
 		player_id_name[info] = str(multiplayer.get_unique_id())
+		print("Test serveur")
+		infoConnectionClient.rpc(info)
+
+@rpc("authority")
+func infoConnectionClient(info):
+	print("Test client")
+
+@rpc("authority")
+func statusServerClient(status):
+	_statusServer(status)
 	
 func connected_to_server():
 	print("Connected to server...")
@@ -45,11 +57,15 @@ func disconnected_from_server():
 	print("disconnected from server...")
 	
 func _on_client_connected(clientId):
-	print(clientId + " : Connected")
+	print(str(clientId) + " : Connected")
+	statusServerClient.rpc(status_serveur)
 	
 func _on_client_disconnected(clientId):
-	print(clientId + " : Deconnected")
-	
+	print(str(clientId) + " : Deconnected")
+
+func _statusServer(status):
+	_statusText.text = status
+
 func _appuyer_connection():
 	var NomActuel = _yourName.text
 	infoConnectionsServer.rpc(NomActuel)
