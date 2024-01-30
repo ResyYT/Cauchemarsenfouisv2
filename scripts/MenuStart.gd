c26c4ff257f1f061660a76216835c18abfabbd17
ResyYT <67598309+ResyYT@users.noreply.github.com>
2024-01-30 14:17:38 +0100

    Ajout connection + options

--- scripts/MenuStart.gd
+++ scripts/MenuStart.gd
@@ -3,10 +3,10 @@ extends Control
 func _ready():
 	if OS.has_feature("dedicated_server"):
 		print("loading server...")
-		get_tree().change_scene_to_file("res://scenes/startup_scene.tscn")
+		get_tree().change_scene_to_file("res://scenes/connection.tscn")
 
 func _on_jouer_pressed():
-	get_tree().change_scene_to_file("res://scenes/startup_scene.tscn")
+	get_tree().change_scene_to_file("res://scenes/connection.tscn")
 
 
 func _on_options_pressed():
--- scripts/Startup.gd
+++ scripts/Startup.gd
@@ -1,11 +1,8 @@
 extends Control
 
-@onready var _statusText = $Panel/VBoxContainer/Status
-@onready var _serverMessagesText = $Panel/VBoxContainer/MessagesFromServer
-@onready var _nombreClient = $Panel/VBoxContainer/ClientConnected
-@onready var _yourName = $Panel/VBoxContainer/YourName
-@onready var nomText = $Panel/VBoxContainer/Nom
-var nombre_client = 0
+@onready var _statusText = $Panel/InformationsStatus/StatusServeur
+@onready var _yourName = $Panel/InformationsConnection/Pseudo
+@onready var _connection = $Panel/BoutonsConnection/Connection
 var player_id_name = {}
 
 func _ready():
@@ -30,80 +27,32 @@ func startClient():
 	multiplayer.connected_to_server.connect(self.connected_to_server)
 	multiplayer.server_disconnected.connect(self.disconnected_from_server)
 	print("creating client...")
-	_updateStatusText("starting Client...")
 	
 	var client = ENetMultiplayerPeer.new()
 	client.create_client("54.37.40.235",1111)
 	multiplayer.multiplayer_peer = client
-	var ip = multiplayer.get_unique_id()
 	
 @rpc("any_peer") #0
-func send_message_to_server(message):
+func infoConnectionsServer(info):
 	if multiplayer.is_server():
-		send_message_to_client.rpc("Demande de connexion envoyé : " + str(message))
-		modifieAffichageNom.rpc()
-
-@rpc("authority") #1
-func send_message_to_client(message:String):
-	_updateServerMessageText(message)
-	
-@rpc("any_peer") #0
-func afficher_nom(Nom):
-	if multiplayer.is_server():
-		modifieAffichageNom.rpc(Nom)
-
-@rpc("authority") #1
-func modifieAffichageNom(Nom):
-	_modifieAffichageNom(str(Nom))
-
-@rpc("any_peer") #0
-func EnleverNombreClient(Nombre):
-	if multiplayer.is_server():
-		print(nombre_client)
-		modifieNombreClient.rpc(Nombre)
-		
-@rpc("any_peer") #0
-func AjouterNombreClient(Nombre):
-	if multiplayer.is_server():
-		print(nombre_client)
-		modifieNombreClient.rpc(Nombre)
-		
-@rpc("authority") #1
-func modifieNombreClient(Nombre):
-	_modifieNombreClient(str(Nombre))
+		player_id_name[multiplayer.get_unique_id()] = str(info)
+		player_id_name[info] = str(multiplayer.get_unique_id())
 	
 func connected_to_server():
 	print("Connected to server...")
-	_updateStatusText("Connected to server...")
 	
 func disconnected_from_server():
 	print("disconnected from server...")
 	
 func _on_client_connected(clientId):
-	nombre_client = nombre_client + 1
-	AjouterNombreClient(nombre_client)
+	print(clientId + " : Connected")
 	
 func _on_client_disconnected(clientId):
-	nombre_client = nombre_client - 1
-	EnleverNombreClient(nombre_client)
+	print(clientId + " : Deconnected")
 	
-func _send_message_to_server():
-	print("Connected")
-	var NomActuel = nomText.text
-	send_message_to_server.rpc(str(multiplayer.get_unique_id()))
-	afficher_nom.rpc(str(NomActuel))
-	
-func _updateStatusText(statusText):
-	_statusText.text = statusText
+func _appuyer_connection():
+	var NomActuel = _yourName.text
+	infoConnectionsServer.rpc(NomActuel)
 
-func _updateServerMessageText(message):
-	_serverMessagesText.text = message
-	
-func _modifieAffichageNom(Nom):
-	_yourName.text = "Pseudo : " + Nom
-	
-func _modifieNombreClient(Nombre):
-	_nombreClient.text = "Nombre de clients : " + Nombre
-
-func _on_return_pressed():
+func _on_bouton_retour_pressed():
 	get_tree().change_scene_to_file("res://scenes/menu_start.tscn")
