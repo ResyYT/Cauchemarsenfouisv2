983f3120928c2477f78b7258a4e6d1a01b642b14
ResyYT <martin.schnidersylvestre@gmail.com>
2024-02-09 22:15:25 +0100

    ajout objets, textures

--- scripts/EchapMenu.gd
+++ scripts/EchapMenu.gd
@@ -3,7 +3,7 @@ extends Control
 signal quit(boolean)
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.connect("false")
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
